a64319b0325f76465afb691d6e5127caeac56560
toshi0607 <s.toshi0607@gmail.com>
2018-03-09 03:34:15 +0900

    bot

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=gshandson-Web-Deploy.pubxml -p:Password=SS2rtrNHTZJ2fhMxft7ZD6ojNKQJffKFJevHp6iTNnKhXnHbHixf3MqaGHGB
+
